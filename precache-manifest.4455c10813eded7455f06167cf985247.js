0e6ccebf3db1c00f46dbd37e081d899c8f1a1e15
Danila Maximov <mr.quze@gmail.com>
2019-06-03 05:40:18 +0300

    Updates

---
+++ precache-manifest.4455c10813eded7455f06167cf985247.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e418f1c8dfe79e896966fe278d750dc",
+    "url": "/kawaii-frontpage/index.html"
+  },
+  {
+    "revision": "dcdd9c3d49f331494eab",
+    "url": "/kawaii-frontpage/static/css/main.1b405891.chunk.css"
+  },
+  {
+    "revision": "95a4c5356e48864cce49",
+    "url": "/kawaii-frontpage/static/js/2.50f4a391.chunk.js"
+  },
+  {
+    "revision": "dcdd9c3d49f331494eab",
+    "url": "/kawaii-frontpage/static/js/main.82c8aea6.chunk.js"
+  },
+  {
+    "revision": "a11b1c16c0640de43664",
+    "url": "/kawaii-frontpage/static/js/runtime~main.74967235.js"
+  },
+  {
+    "revision": "b8cbac9fbf7fa09d24c30817457410cf",
+    "url": "/kawaii-frontpage/static/media/loading.b8cbac9f.gif"
+  }
+]);
